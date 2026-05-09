81a9969f6e8eae3d39fe9c942d1922b6bcb8dc12
Nikolay Kulikov <nikolayof23@gmail.com>
2026-05-09 16:32:03 +0300

    lib: relays: add struct relay and struct relays_service
    These structures are the main ones for working with relays, they will be
    needed to call library functions.

--- src/relays/relays_lib.h
+++ src/relays/relays_lib.h
@@ -1,7 +1,23 @@
 #ifndef RELAYS_LIB_H
 #define RELAYS_LIB_H
 
+#include <stdint.h>
 
+
+struct relay {
+	bool open;			/* true - open, false - close */
+	int16_t gpio;			/* aka pin; < 0 mean unset */
+	uint32_t timer;			/* the millis() value when @state was changed */
+	int16_t frequency;		/* freq < 0 -- do not use it and @freq_period */
+	uint16_t freq_period;
+};
+
+
+struct relays_service {
+	struct relay *relays;
+
+	uint16_t relays_count;
+};
 
 
 #endif /* RELAYS_LIB_H */
